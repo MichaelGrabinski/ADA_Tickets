db60244f773c3460051ba9216f895e35655ad87b
MichaelGrabinski <mikeyboy06898@gmail.com>
2025-09-12 08:54:12 -0400

    up

--- riders - Copy/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-#@admin.register(ADARider)
-class ADARiderAdmin(admin.ModelAdmin):
-    list_display = ('last_name','first_name','ada_id','inactive','zip_code')
-    search_fields = ('first_name','last_name','ada_id')
-#admin.site.register(ADATicketPurchase)
-#admin.site.register(RiderNote)
